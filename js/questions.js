bb4d90dbc6ac53d02a67616882dab30ab6191509
ArYaNLeGeND <87259561+ArYaNLeGeND@users.noreply.github.com>
2021-07-11 01:02:02 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,125 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was Queen Victoria elected to be emperor?",
+    answer: "1876",
+    options: [
+      "1885",
+      "1876",
+      "1813",
+      "1812"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who suggested the process of Prastawana at the constitution Prarup Samiti? ",
+    answer: "Jawaharlal Nehru",
+    options: [
+      "BhimRao Ambedkar",
+      "Muhammed Sanaullah",
+      "Mahatma Gandhi",
+      "Jawaharlal Nehru"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does UNICEF stand for?",
+    answer: "United Nations Children’s Fund",
+    options: [
+      "United Nations International Children's Education Fund",
+      "United Nations Children’s Fund",
+      "United Nations Computrised Formation",
+      "None of These"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does GIMP stand for?",
+    answer: "General Image Manipulation Program",
+    options: [
+      "General Idea Manipulation Product",
+      "General Image Manipulation Program",
+      "General Info Maintenance Program",
+      "Group Intelligence Manipulation Population"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does MICR stand for?",
+    answer: "Magenatic Ink Character Reader",
+    options: [
+      "Monthly Income Collected Reader",
+      "Money Interface Changing Rule",
+      "Magenatic Ink Character Reader",
+      "Magnet Info Changing Rule"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Option 1",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+
+  // {
+  //   numb: 7,
+  //   question: "Your Question is Here",
+  //   answer: "option 2",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+
+
+  // {
+  //   numb: 8,
+  //   question: "Your Question is Here",
+  //   answer: "option 3",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+
+
+  // {
+  //   numb: 9,
+  //   question: "Your Question is Here",
+  //   answer: "option 4",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+
+
+
+  // {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Option 1",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+
+];
